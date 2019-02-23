93a20666f0b0d593a401b103567c3e3f14ac808a
sophianetSun <sophianet.sun@gmail.com>
2019-02-23 15:07:35 +0900

    fix main bug

--- main.py
+++ main.py
@@ -17,19 +17,21 @@ class TestFPProcess(unittest.TestCase):
         self.ser.close()
 
     def test_set_dormant(self):
-        self.ser.write(fp.set_dormant_state())
-        self.assertEqual(self.ser.read(8),
+        res = self.ser.write(fp.set_dormant_state())
+        self.assertEqual(res,
             [HEAD, 0x2C, 0, 0, 0, 0, 0x2C^0, TAIL],
             'should dormant be set')
 
     def test_add_read_mode(self):
-        self.ser.write(fp.fingerprint_mode('read'))
-        self.assertEqual(self.ser.read(8),
+        res = self.ser.write(fp.fingerprint_mode('read'))
+        self.assertEqual(res,
             [HEAD, 0x2D, 0, 1, fp.ACK_SUCCESS, 0, 0x2D^0, TAIL],
             'should read add mode success')
 
     def test_del_all_users(self):
-        self.ser.write(fp.del_all_users())
-        self.assertEqual(self.ser.read(8),
+        res = self.ser.write(fp.del_all_users())
+        self.assertEqual(res,
             [HEAD, 0x05, 0, 0, fp.ACK_SUCCESS, 0, 0x05^0, TAIL])
-    
+
+if __name__ == '__main__':
+    unittest.main()
