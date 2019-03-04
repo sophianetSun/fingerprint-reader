36b3cb010674e732d3994e1a98b38dc302c5b914
sophianetSun <sophianet.sun@gmail.com>
2019-03-04 22:34:01 +0900

    prototype is done

--- fingerprint.py
+++ fingerprint.py
@@ -55,7 +55,7 @@ CMD_DOWN_COMP_MANY = 0x43
 CMD_DOWN_COMP = 0x44
 
 class User:
-    def __init__(self, high, low, privilege):
+    def __init__(self, high, low, privilege=None):
         self.high = high
         self.low = low
         self.privilege = privilege
@@ -357,7 +357,143 @@ class FingerPrintReader:
 
     def download_eigenvalue(self):
         cmd_buf = [CMD_EXT_EGV, 0, 0, 0, 0]
+        res = self.tx_rx_cmd(cmd_buf, 8, 0.1)
+        if res == ACK_TIMEOUT:
+            return  ACK_TIMEOUT
+        elif res == ACK_SUCCESS and self.rx_buf[4] == ACK_SUCCESS:
+            data_len = int.from_bytes(self.rx_buf[2:4], 'big')
+            packet = self.ser.read(data_len + 3)
+            if check_packet(packet) == ACK_SUCCESS:
+                eigenvalues = packet[4:-2]
+                return eigenvalues
+            else:
+                return ACK_FAIL
 
+        else:
+            return ACK_FAIL
+
+    def get_moudle_version(self):
+        cmd_buf = [CMD_VERSION, 0, 0, 0, 0]
+        header = self.tx_rx_cmd(cmd_buf, 8, 0.1)
+        if header == ACK_SUCCESS:
+            data_len = int.from_bytes(self.rx_buf[2:4], 'big')
+            packet = self.ser.read(data_len + 3)
+            if check_packet(packet) == ACK_SUCCESS:
+                version = packet[1:-2].decode()
+                return version
+        else:
+            return ACK_FAIL
+
+    def up_comp_fingerprint(self, eigenval):
+        """
+        module download eigenvalues and comparison
+        :param eigenval: binary data
+        :return:
+        """
+        byte_len = len(eigenval).to_bytes(2, 'big')
+        cmd_header = [CMD_DOWN_COMP, byte_len[0], byte_len[1], 0, 0]
+        self.tx_rx_cmd(cmd_header, 8, 0.1)
+        packet = bytes([CMD_HEAD, 0, 0, 0]) + eigenval
+        chk = get_chksum(packet[1:])
+        packet += bytes([chk, CMD_TAIL])
+
+        res = self.ser.write(packet)
+        if res[4] == ACK_SUCCESS:
+            return ACK_SUCCESS
+        elif res[4] == ACK_TIMEOUT:
+            return ACK_TIMEOUT
+        else:
+            return ACK_FAIL
+
+    def up_comp_by_id(self, eigenval, user_id):
+        byte_len = int(len(eigenval)).to_bytes(2, 'big')
+        header_buf = [CMD_HEAD, CMD_DOWN_COMP_ONE, byte_len[0], byte_len[1], 0, 0]
+        chk = get_chksum(header_buf[1:])
+        header_buf.append(chk)
+        header_buf.append(CMD_TAIL)
+        self.ser.write(header_buf)
+        byte_id = self.id_to_byte(user_id)
+        packet_buf = bytes([CMD_HEAD, byte_id[0], byte_id[1], 0]) + eigenval
+        chk = get_chksum(packet_buf[1:])
+        packet_buf += bytes([chk, CMD_TAIL])
+        self.ser.write(packet_buf)
+        res = self.ser.read(8)
+        if res[4] == ACK_SUCCESS:
+            return ACK_SUCCESS
+        else:
+            return ACK_FAIL
+
+    def up_comp_many(self, eigenval):
+        byte_len = int(len(eigenval)).to_bytes(2, 'big')
+        cmd_header = [CMD_HEAD, CMD_DOWN_COMP_MANY, byte_len[0], byte_len[1], 0, 0]
+        chk = get_chksum(cmd_header[1:])
+        cmd_header += [chk, CMD_TAIL]
+        self.ser.write(cmd_header)
+        packet_buf = bytes([CMD_HEAD, 0, 0, 0]) + eigenval
+        chk = get_chksum(packet_buf[1:])
+        packet_buf += bytes([chk, CMD_TAIL])
+        self.ser.write(packet_buf)
+        res = self.ser.read(8)
+        if res[4] == ACK_NO_USER:
+            return ACK_NO_USER
+        else:
+            return User(res[2], res[3], res[4])
+
+    def down_db_by_id(self, user_id):
+        byte_id = self.id_to_byte(user_id)
+        cmd_buf = [CMD_HEAD, CMD_UP_ONE_DB, byte_id[0], byte_id[1], 0, 0]
+        chk = get_chksum(cmd_buf[1:])
+        cmd_buf += [chk, CMD_TAIL]
+        self.ser.write(cmd_buf)
+        header = self.ser.read(8)
+        if header[4] == ACK_SUCCESS:
+            data_len = int.from_bytes(header[2:4], 'big')
+            res = self.ser.read(data_len + 3)
+            user = User(res[1], res[2], res[3])
+            user.eigenvalue = res[4:-2]
+            return user
+        elif header[4] == ACK_NO_USER:
+            return ACK_NO_USER
+        else:
+            return ACK_FAIL
+
+    def up_eigen_save_by_id(self, user_id, eigenval):
+        byte_len = int(len(eigenval)).to_bytes(2, 'big')
+        header_buf = [CMD_HEAD, CMD_DOWN_ONE_DB, byte_len[0], byte_len[1], 0]
+        chk = get_chksum(header_buf[1:])
+        header_buf += [chk, CMD_TAIL]
+        self.ser.write(header_buf)
+        byte_id = self.id_to_byte(user_id)
+        packet = bytes([CMD_HEAD, byte_id[0], byte_id[1], 2]) + eigenval
+        chk = get_chksum(packet[1:])
+        packet += ([chk, CMD_TAIL])
+        self.ser.write(packet)
+        res = self.ser.read(8)
+        if res[4] == ACK_SUCCESS:
+            return User(res[2], res[3])
+        else:
+            return ACK_FAIL
+
+    def get_all_user_info(self):
+        cmd_buf = [CMD_ALL_USR, 0, 0, 0, 0]
+        header = self.tx_rx_cmd(cmd_buf, 8, 0.1)
+        if header == ACK_SUCCESS:
+            data_len = int.from_bytes(self.rx_buf[2:4], 'big')
+            packet = self.ser.read(data_len + 3)
+            if check_packet(packet) == ACK_SUCCESS:
+                user_num = int.from_bytes(packet[2:4], 'big')
+                users = []
+                for i in range(user_num):
+                    idx = i * 3
+                    high = packet[idx]
+                    low = packet[idx + 1]
+                    pri = packet[idx + 2]
+                    users.append(User(high, low, pri))
+                return users
+            else:
+                return ACK_FAIL
+        else:
+            return ACK_FAIL
 
 
 def check_packet(packet):
@@ -372,88 +508,9 @@ def check_packet(packet):
     else:
         return ACK_FAIL
 
-def upload_extract_eigenvalue():
-    return bytes([CMD_HEAD, CMD_UP_EXT, 0, 0,
-                  0, 0, CMD_UP_EXT^0, CMD_TAIL])
 
-
-def download_eigenvalues_comp_fingerprint(eigenvalues):
-    egval_length = len(eigenvalues).to_bytes(2, 'big')
-    header = bytes([CMD_HEAD, CMD_DOWN_ACQ, egval_length[0], egval_length[1],
-              0, 0, CMD_DOWN_ACQ^0, CMD_TAIL])
-    packet = bytes[CMD_HEAD, 0, 0, 0] + eigenvalues + 0^eigenvalues[1] + b'0xF5'
-    return header + packet
-
-
-def download_eigenvalues_comp_db_by_id(user_id, eigenvalues):
-    id_high, id_low = id_to_byte(user_id)
-    egval_length = len(eigenvalues).to_bytes(2, 'big')
-
-    header = bytes([CMD_HEAD, CMD_DOWN_COMP, egval_length[0], egval_length[1],
-                    0, 0, CMD_DOWN_COMP^0, CMD_TAIL])
-    packet = bytes([CMD_HEAD, id_high, id_low, 0]) + eigenvalues + bytes([id_high^eigenvalues[1], CMD_TAIL])
-
-    return header + packet
-
-
-def download_eigenvalues_comp_db_many(eigenvalues):
-    egval_length = len(eigenvalues).to_bytes(2, 'big')
-
-    header = bytes([CMD_HEAD, CMD_DOWN_COMP_N, egval_length[0], egval_length[1],
-                    0, 0, CMD_DOWN_COMP_N^0, CMD_TAIL])
-    packet = bytes([CMD_HEAD, 0, 0, 0]) + eigenvalues + bytes([0^eigenvalues[1], CMD_TAIL])
-
-    return header + packet
-
-
-def upload_dsp_by_id(user_id):
-    id_high, id_low = id_to_byte(user_id)
-
-    return bytes([CMD_HEAD, CMD_UP_EGV, id_high, id_low, 0, 0, CMD_UP_EGV^0, CMD_TAIL])
-
-
-def download_eigenvalue_save_by_id(user_id, eigenvalues, user_privilege=1):
-    id_high, id_low = id_to_byte(user_id)
-    egval_length = len(eigenvalues).to_bytes(2, 'big')
-
-    header = bytes([CMD_HEAD, CMD_DOWN_SAVE, egval_length[0], egval_length[1],
-                    0, 0, CMD_DOWN_SAVE^0, CMD_TAIL])
-    packet = bytes([CMD_HEAD, id_high, id_low, user_privilege]) + \
-             eigenvalues + bytes([id_high^eigenvalues[1], CMD_TAIL])
-
-    return header + packet
-
-
-def all_user_id_privilege():
-    return bytes([CMD_HEAD, CMD_ALL_USR, 0, 0,
-                  0, 0, 0^CMD_ALL_USR, CMD_TAIL])
-
-
-def set_timeout(value):
-    """
-    If the value is 0, fingerprint acquisition process will keep continue, if no fingerprint prees on.
-    otherwise timeout set be value * (0.2~0.3)s
-    :param value: 0-255
-    :return: 8 bytes
-    """
-    if value < 0 or 255 < value:
-        raise AttributeError('argument value out of range')
-    return bytes([CMD_HEAD, CMD_TIMEOUT, 0, value,
-                  0, 0, CMD_TIMEOUT^0, CMD_TAIL])
-
-
-def get_timeout():
-    return bytes([CMD_HEAD, CMD_TIMEOUT, 0, 1])
-
-
-#assert bytes([0xF5, 0x2C, 0, 0, 0, 0, 0x2C^0, 0xF5]) == set_dormant_state(), 'Dormant State Response Error!'
-assert bytes([0xF5, 0x2D, 0, 0, 0, 0, 0x2D^0, 0xF5]) == fingerprint_mode('set', True), 'Set Fingerprint add mode allow repeat'
-assert bytes([0xF5, 0x2D, 0, 0x01, 0, 0, 0x2D^0, 0xF5]) == fingerprint_mode('set', False), 'Set Fingerprint add mode prohibit repeat'
-assert bytes([0xF5, 0x2D, 0, 0, 0x01, 0, 0x2D^0, 0xF5]) == fingerprint_mode('read'), 'Read Fingerprint add mode'
-assert bytes([0xF5, 0x01, 0, 0, 0x01, 0, 0x01^0, 0xF5]) == add_fingerprint_first('0'), 'Add first Fingerprint should be correct'
-assert bytes([0xF5, 0x02, 0, 0, 0x01, 0, 0x02^0, 0xF5]) == add_fingerprint_second('0'), 'Add second Fingerprint should be correct'
-assert bytes([0xF5, 0x03, 0, 0, 0x01, 0, 0x03^0, 0xF5]) == add_fingerprint_third('0'), 'Add third Fingerprint should be correct'
-assert bytes([0xF5, 0x04, 0, 0, 0, 0, 0x04^0, 0xF5]) == del_specified_user('0'), 'Delete specified user by user id'
-assert bytes([0xF5, 0x05, 0, 0, 0, 0, 0x05^0, 0xF5]) == del_all_users(), 'Del all users'
-assert bytes([0xF5, 0x0B, 0, 0, 0, 0, 0x0B^0, 0xF5]) == compare_by_id('0'), 'compare with 0 should be correct'
-
+def get_chksum(data):
+    chk = 0
+    for byte in data:
+        chk ^= byte
+    return chk
